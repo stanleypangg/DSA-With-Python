3119f6cd925e582a7d4c1f90969f2bf9c58af628
Stanley Pang <stanley.pang@mail.utoronto.ca>
2025-05-02 20:41:36 -0400

    bubble sort

---
+++ sorting/bubble_sort.py
@@ -0,0 +1,5 @@
+def bubble_sort(lst: list[int]) -> None:
+    for i in range(len(lst)):
+        for j in range(len(lst) - i - 1):
+            if lst[j] > lst[j + 1]:
+                lst[j], lst[j + 1] = lst[j + 1], lst[j]
