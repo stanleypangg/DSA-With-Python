54b99fa3fdf0f4425072abb3efe3c4c4bc84dd17
Stanley Pang <stanley.pang@mail.utoronto.ca>
2025-05-04 00:40:05 -0400

    bubble-sort

--- sorting/bubble_sort.py
+++ sorting/bubble_sort.py
@@ -1,5 +1,29 @@
-def bubble_sort(lst: list[int]) -> None:
-    for i in range(len(lst)):
-        for j in range(len(lst) - i - 1):
+# Ascending
+def bubble_sort(lst: list[int]) -> list[int]:
+    n = len(lst)
+    # Loop n times
+    for i in range(n):
+        swapped = False
+        
+        # Compare each adjacent pairs of elements
+        for j in range(n - i - 1):
             if lst[j] > lst[j + 1]:
                 lst[j], lst[j + 1] = lst[j + 1], lst[j]
+                swapped = True
+        
+        if not swapped:
+            break
+    
+    return lst
+
+# Descending
+def bubble_sort_descending(lst: list[int]) -> list[int]:
+    n = len(lst)
+    for i in range(n):
+        # End of list will be sorted
+        # Only run as many iterations as needed
+        for j in range(n - i - 1):
+            if lst[j] < lst[j + 1]:
+                lst[j], lst[j + 1] = lst[j + 1], lst[j]
+    
+    return lst
